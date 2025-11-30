71753d381fd113341820ae3ef50c3e24bd5ed8c3
NudleOS Creator <shawn.jung.f21@gmail.com>
2025-11-29 17:58:55 -0800

    edit

---
+++ APPDATA/Desktop Applications/Orion Terminal/main.sh
@@ -0,0 +1,11 @@
+#We will import the U-Shell that uses every single non-UNIX or non-DOS command. It only has tree, and about 120 buiilt-in commands. In main.py, this file will be implemented fully into a application.
+
+# Import the U-Shell
+from UShell import UShell
+
+# Create an instance of the U-Shell
+shell = UShell()
+
+# Start the shell
+shell.start()
+#end
